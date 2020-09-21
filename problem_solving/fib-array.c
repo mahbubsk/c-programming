adeb433057a761fc0874109f9e55ca144dae3b1b
MD Mahbub SK <mahbubsk>
2020-09-21 06:11:37 +0600

    problem is not pain

--- problem_solving/fib-array.c
+++ problem_solving/fib-array.c
@@ -1,18 +1,25 @@
 #include<stdio.h>
 
 int main(){
-    int fib[60];
-    int i, j;
-    int input;
+    int input,i, j;
+    long long fib[60], input2;
+    long long  a=0, b=1, c=0;
+    
 
     scanf("%d",&input);
 
     for(i=0; i<input; i++){
-        scanf("%d",&fib[i]);
-    }
-
-    for(i=0; i<input; i++){
-        printf("fib(%d) = %d\n",i,fib[i]);
+        scanf("%lld",&input2);
+        for(j=0; j<=input2; j++){
+            a=b;
+            b=c;
+            c=a+b;
+        }
+        printf("Fib(%lld) = %lld\n",input2,b);
+        a=0;
+        b=1;
+        c=0;
+        
     }
     return 0;
 }
